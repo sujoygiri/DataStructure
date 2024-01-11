e221df783597cb4fcd464c71d473757d75014ba0
SUJOY GIRI <sujoy.giri@infosys.com>
2024-01-11 10:20:06 +0530

    implemented merge sort

---
+++ Algorithom/merge_sort.js
@@ -0,0 +1,37 @@
+/**
+ * Implement merge sort
+ * @param {number[]} arr
+ */
+function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
+    let middle = Math.floor(arr.length / 2);
+    let leftSubArr = arr.slice(0, middle);
+    let rightSubArr = arr.slice(middle);
+
+    return merge(mergeSort(leftSubArr), mergeSort(rightSubArr));
+}
+
+/** 
+ * @param {number[]} leftSubArr  
+ * @param {number[]} rightSubArr 
+*/
+function merge(leftSubArr, rightSubArr) {
+    let mergedArr = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < leftSubArr.length && rightIndex < rightSubArr.length) {
+        if (leftSubArr[leftIndex] < rightSubArr[rightIndex]) {
+            mergedArr.push(leftSubArr[leftIndex]);
+            ++leftIndex;
+        } else {
+            mergedArr.push(rightSubArr[rightIndex]);
+            ++rightIndex;
+        }
+    }
+    console.log({leftSubArr,rightSubArr,mergedArr});
+    return mergedArr.concat(leftSubArr.slice(leftIndex)).concat(rightSubArr.slice(rightIndex))
+}
+
+console.log('After sort:- ',mergeSort([6,4,4,1]));
